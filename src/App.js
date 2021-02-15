7e3e627e81678157be0e481d534f7b4c0ddc8280
Wambura01 <brianwambura96@gmail.com>
2021-02-15 14:31:52 +0300

    Integrated the face recognition functionality by using the clarifai API for their face detection model. Styled the image container to fit better in the app window

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+import Particles from "react-particles-js";
+import Clarifai, { COLOR_MODEL } from "clarifai";
+
+import Navigation from "./Components/Navigation/navigation.component";
+import Logo from "./Components/Logo/logo.component";
+import Rank from "./Components/Rank/Rank.component";
+import ImageLinkForm from "./Components/ImageLinkForm/imagelinkform.component";
+import FaceRecognition from "./Components/FaceRecognition/facerecognition.component";
+
+//using the clarifai api to detect faces in images
+const app = new Clarifai.App({
+  apiKey: "65622a812f0843049fe23957ef31da60",
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+    },
+  },
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: "",
+      imageUrl: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function (response) {
+        // do something with response
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+      },
+      function (err) {
+        // there was an error
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
